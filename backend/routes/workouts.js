c2c6c82b71700727a30886f6c2dfea006b3f81ff
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-12-13 21:16:43 -0600

    created basic templates for workout routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -5,5 +5,34 @@ const express = require('express')
 const router = express.Router()
 
 
+//ROUTES--
+
+//GET all workouts
+router.get('/', (req, res)=>{
+    res.json({message: "GET all workouts"})
+})
+
+//GET single workout
+router.get('/:id', (req, res) => {
+    res.json({message: "GET single workout"})
+})
+
+//CREATE a workout
+router.post("/", (req, res) => {
+    res.json({message: "CREATE a workout"})
+})
+
+
+//DELETE a workout
+router.delete("/:id", (req, res) => {
+    res.json({message: "DELETE a workout"})
+})
+
+//UPDATE a workout
+router.patch("/:id", (res, req) => {
+    res.json({message: "UPDATE a workout"})
+})
+
+
 //allows us to use router outside of workouts.js
 module.exports() = router
